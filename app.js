2a99e684d3c862b451f10a65eae9542181200ecf
Shamzic <shamery@free.fr>
2020-04-15 17:57:35 +0200

    connect to mongoDB working

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+// body parser update
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// Middlewares
+
 
 // ROUTES
 app.get('/', (req, res)=> {
   res.send("We are on home");
+});
+
+app.get('/posts', (req, res)=> {
+  res.send("We are on posts");
+});
+
+
+// Connect to DB
+mongoose.connect('mongodb://admin:tototo1@ds235833.mlab.com:35833/rest',  { useNewUrlParser: true, useUnifiedTopology: true },  () => {
+  console.log("connected to DB !")
 })
+
 
 // listening the server
 app.listen(3000);
